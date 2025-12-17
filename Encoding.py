8c64f52665663fe7b32d9fde29a4fcd8b0fbdaac
Julien Abrant <j.abrant@proton.me>
2025-12-17 09:21:35 +0100

    Fonctions d'encodage

---
+++ Encoding.py
@@ -0,0 +1,8 @@
+def utf8_encode(text):
+    return list(text.encode('utf-8', errors='ignore'))
+def utf8_decode(byte_list):
+    return bytes(byte_list).decode('utf-8', errors='ignore')
+def hex_encode(byte_list):
+    return bytes(byte_list).hex()
+def hex_decode(hex_string):
+    return list(bytes.fromhex(hex_string))
